98da526ac13a76a843f523f36354d0dff830a272
Rishi Varma Kalidindi <rishivarmak.developer@gmail.com>
2023-03-05 20:57:43 -0500

    added null check for body page

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ let bodyPage = document.getElementById("body")
 let para = document.getElementById("para")
 let title = document.getElementById("title")
 
+if(bodyPage == null) {
+    console.log("BodyPage is NULL")
+}
+
 bodyPage.addEventListener("load", bodyOnLoad)
 
 let bodyOnLoad = function() {
